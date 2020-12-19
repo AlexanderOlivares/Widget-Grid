c3ecd37568f1fd929f68542784dd1166aa7169c1
Alex Olivares <alexander.b.olivares@gmail.com>
2020-12-19 01:10:02 -0600

    grid and notepad created

---
+++ main.js
@@ -0,0 +1,53 @@
+let clock = document.getElementById('clock');
+let calculator = document.getElementById('calculator');
+let weather = document.getElementById('weather');
+let notes = document.getElementById('notes');
+
+function addBorder(tab){
+    if (tab === clock){
+        calculator.style.border = '';
+        notes.style.border = '';
+        weather.style.border = '';
+        clock.style.border = '2px solid cyan'
+    } else if (tab === calculator){
+        calculator.style.border = '2px solid cyan';
+        notes.style.border = '';
+        weather.style.border = '';
+        clock.style.border = '';
+        } else if (tab === weather){
+            calculator.style.border = '';
+            notes.style.border = '';
+            weather.style.border = '2px solid cyan';
+            clock.style.border = ''
+            } else if (tab === notes){
+                calculator.style.border = '';
+                notes.style.border = '2px solid cyan';
+                weather.style.border = '';
+                clock.style.border = ''
+            }
+}
+
+clock.addEventListener('click', ()=> {
+    addBorder(clock);
+})
+
+calculator.addEventListener('click', ()=> {
+    addBorder(calculator);
+})
+
+weather.addEventListener('click', ()=> {
+    addBorder(weather);
+})
+
+notes.addEventListener('click', ()=> {
+    addBorder(notes);
+})
+
+
+let textBox = document.getElementById('textBox');
+let noteButon = document.getElementById('noteButton');
+let note1 = document.getElementById('note1');
+
+noteButon.addEventListener('click', ()=> {
+    note1.innerHTML = textBox.value;
+})
