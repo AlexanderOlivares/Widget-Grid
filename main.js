a84c8875310ff6b5fa0f77878a5d02e76ea5db77
Alex Olivares <alexander.b.olivares@gmail.com>
2020-12-21 11:18:21 -0600

    current weather data added

--- main.js
+++ main.js
@@ -44,7 +44,7 @@ notes.addEventListener('click', ()=> {
 })
 
 
-// *** notes section below *** 
+// ******************* notes section below ************************
 let notesAddNote = document.getElementById('notesAddNote');
 let notesTextBox = document.getElementById('notesTextBox');
 let notesPostNoteButton = document.getElementById('notesPostNoteButton');
@@ -114,9 +114,114 @@ notesClearNotesButton.addEventListener('click', ()=> {
 })
 
 
+// ****************** weather section below ************************8
+let weatherRefreshButton = document.getElementById('weatherRefreshButton');
+let weatherEnterCity = document.getElementById('weatherEnterCity');
+let weatherGoButton = document.getElementById('weatherGoButton');
+let weatherCurrentTemp = document.getElementById('weatherCurrentTemp');
+let weatherCurrentIcon = document.getElementById('weatherCurrentIcon');
+let weatherCurrentDescription = document.getElementById('weatherCurrentDescription');
+let weatherFeelsLike = document.getElementById('weatherFeelsLike');
+let weatherWind = document.getElementById('weatherWind');
+let weatherHumididty = document.getElementById('weatherHumidity');
+
+const weatherGetCurrents = () => {
+fetch('https://api.openweathermap.org/data/2.5/onecall?lat=30.382580&lon=-97.710243&appid=89487f45423ccbfbdd6e1ea526f5177f')
+    .then(response=> response.json())
+    .then(data=> {
+        let temp = data.current.temp;
+        let icon = data.current.weather[0]['icon'];
+        let description = data.current.weather[0]['description'];
+        let feelsLike = data.current.feels_like;
+        let wind = data.current.wind_speed;
+        let humidity = data.current.humidity;
+        console.log(data);
+        weatherCurrentTemp.innerHTML = Math.round(temp * 9 / 5 - 459.67) + '\u00B0';
+        weatherCurrentIcon.innerHTML =  `<img src="http://openweathermap.org/img/wn/${icon}@2x.png">`
+        weatherCurrentDescription.innerHTML = description;
+        weatherFeelsLike.innerHTML = `feels like ${Math.round(feelsLike * 9 / 5 - 459.67)}\u00b0`;
+        weatherWind.innerHTML = `${Math.round(wind)}mph`
+        weatherHumididty.innerHTML = `humidity ${humidity}%`
+    })
+    .catch(errror=> console.error('problem occured'))
+}
+
+weatherGetCurrents();
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //30.3871° N, 97.7037° W
+//30.382580 -97.710243
+// f = k × 9/5 - 459.67
+/*
+"id": 4671654,
+        "name": "Austin",
+        "state": "TX",
+        "country": "US",
+        "coord": {
+            "lon": -97.743057,
+            "lat": 30.267151
+*/
+
+//weatherCurrentTemp.innerHTML =
 
 
 
